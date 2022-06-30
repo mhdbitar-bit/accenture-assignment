2efd062a3697f85939fe17a78fd18c6057cf0063
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-06-30 22:06:01 +0300

    Improve readability with helper method

--- accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
+++ accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
@@ -35,7 +35,7 @@ final class RemoteCategoryLoaderTests: XCTestCase {
         sut.load { capturedError.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedError, [.connecitivy])
     }
@@ -55,6 +55,10 @@ final class RemoteCategoryLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
