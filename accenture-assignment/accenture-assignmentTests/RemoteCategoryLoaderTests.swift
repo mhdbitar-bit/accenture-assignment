90ec80717a2630c0547bbe3d4ce21836540a11b4
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-06-30 20:39:10 +0300

    Move test logic to a spectate type `HTTPClientSpy`

--- accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
+++ accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
@@ -2,19 +2,42 @@
 import XCTest
 
 class RemoteCategoryLoader {
-    
+    func load() {
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
 class HTTPClient {
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteCategoryLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteCategoryLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        let sut = RemoteCategoryLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
