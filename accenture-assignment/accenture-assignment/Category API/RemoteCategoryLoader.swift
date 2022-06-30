60eb0d2d6147c68e694085b6d7481e4333455353
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-06-30 22:36:57 +0300

    Delivers invalid data error on 200 HTTP response with invalid json

--- accenture-assignment/accenture-assignment/Category API/RemoteCategoryLoader.swift
+++ accenture-assignment/accenture-assignment/Category API/RemoteCategoryLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol HTTPClient {
-    typealias HTTPClientResult = Result<HTTPURLResponse, Error>
+    typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
--- accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
+++ accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
@@ -55,6 +55,18 @@ final class RemoteCategoryLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteCategoryLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+       
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCategoryLoader, client: HTTPClientSpy) {
@@ -70,7 +82,7 @@ final class RemoteCategoryLoaderTests: XCTestCase {
             messages.map { $0.url }
         }
         
-    func get(from url: URL, completion: @escaping (Result<HTTPURLResponse, Error>) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
@@ -78,14 +90,14 @@ final class RemoteCategoryLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
