08a60e341eca295a416caec6e8def23bb3dacb5b
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-06-30 20:54:26 +0300

    Refactor test to remove duplication

--- accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
+++ accenture-assignment/accenture-assignmentTests/RemoteCategoryLoaderTests.swift
@@ -19,31 +19,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class RemoteCategoryLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url..com")!
-        let client = HTTPClientSpy()
-        _ = RemoteCategoryLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteCategoryLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCategoryLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteCategoryLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
