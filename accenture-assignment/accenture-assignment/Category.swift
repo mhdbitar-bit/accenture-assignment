97ba8f77d58413e3115a02b8c28bd281ba51c8a9
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-06-30 20:15:28 +0300

    Create the category model

---
+++ accenture-assignment/accenture-assignment/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 6/30/22.
+//
+
+import Foundation
+
+struct Category {
+    let id: Int
+    let name: String
+}
