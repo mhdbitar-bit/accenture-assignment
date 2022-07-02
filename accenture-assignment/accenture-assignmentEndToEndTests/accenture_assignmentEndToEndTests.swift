1c743f372c1530ef4728caef896331b6e5122bd8
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 15:17:29 +0300

    Add end-to-end api test in a separate test target

---
+++ accenture-assignment/accenture-assignmentEndToEndTests/accenture_assignmentEndToEndTests.swift
@@ -0,0 +1,57 @@
+//
+//  accenture_assignmentEndToEndTests.swift
+//  accenture-assignmentEndToEndTests
+//
+//  Created by Mohammad Bitar on 7/2/22.
+//
+
+@testable import accenture_assignment
+import XCTest
+
+
+class accenture_assignmentEndToEndTests: XCTestCase {
+    
+    func test_endToEndServerGETCategoriesResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://private-anon-f51dfcda52-androidtestmobgen.apiary-mock.com/categories")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteCategoryLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receviedResult: CategoryResult?
+        loader.load { result in
+            receviedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        
+        switch receviedResult {
+        case let .success(categories)?:
+            XCTAssertEqual(categories.count, 3, "Expected 3 items in the test account category")
+            XCTAssertEqual(categories[0], expectedItem(at: 0))
+            XCTAssertEqual(categories[1], expectedItem(at: 1))
+            XCTAssertEqual(categories[2], expectedItem(at: 2))
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful categories result, got \(error) instead")
+        
+        default:
+            XCTFail("Expected successful categories result, got no result instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func expectedItem(at index: Int) -> CategoryItem {
+        CategoryItem(id: id(at: index), name: name(at: index))
+    }
+    
+    private func id(at index: Int) -> Int {
+        return [0, 1, 2][index]
+    }
+    
+    private func name(at index: Int) -> String {
+        return ["Books", "Houses", "Characters"][index]
+    }
+}
