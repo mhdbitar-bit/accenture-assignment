9a6f5ff1c4ded28874fe5992f1cb585c710e52c6
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 23:13:55 +0300

    LocalCategoryLoader does not message store upon creation (before validating the cache category)

---
+++ accenture-assignmentTests/Category Cahce/ValidateCategoriesCacheUseCaseTests.swift
@@ -0,0 +1,21 @@
+@testable import accenture_assignment
+import XCTest
+
+final class ValidateCategoriesCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalCategoryLoader, store: CategoryStoreSpy) {
+        let store = CategoryStoreSpy()
+        let sut = LocalCategoryLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeacks(store, file: file, line: line)
+        trackForMemoryLeacks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
