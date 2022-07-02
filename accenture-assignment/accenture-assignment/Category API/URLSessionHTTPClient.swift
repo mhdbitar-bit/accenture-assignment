1be5545c3141e360fd017f7af11b832eaa560ee0
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 14:27:17 +0300

    Move `URLSessionHTTPClient` to it's file in production

---
+++ accenture-assignment/accenture-assignment/Category API/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/2/22.
+//
+
+import Foundation
+
+class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+            
+        }.resume()
+    }
+}
