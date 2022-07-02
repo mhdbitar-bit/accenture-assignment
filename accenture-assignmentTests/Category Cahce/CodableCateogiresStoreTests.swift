a93db575786f488ecf3d3f87b55117b58f425df8
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 00:47:25 +0300

    Retrieving from empty cache twice deliver same empty result (no side-effects)

--- accenture-assignmentTests/Category Cahce/CodableCateogiresStoreTests.swift
+++ accenture-assignmentTests/Category Cahce/CodableCateogiresStoreTests.swift
@@ -12,6 +12,7 @@ final class CodableCateogiresStoreTests: XCTestCase {
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableCategoriesStore()
         let exp = expectation(description: "Wait for cache retrieval")
+        
         sut.retrieve { result in
             switch result {
             case .empty:
@@ -22,6 +23,27 @@ final class CodableCateogiresStoreTests: XCTestCase {
             }
             
             exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableCategoriesStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                
+                default:
+                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+                }
+            
+                exp.fulfill()
+            }
         }
         
         wait(for: [exp], timeout: 1.0)
