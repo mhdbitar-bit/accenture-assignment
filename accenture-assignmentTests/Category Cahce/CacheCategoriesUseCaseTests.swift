28ced60346fc72f4a9f5a31e51c03f5488ff6abf
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 18:24:11 +0300

    Save command request cache deletion

--- accenture-assignmentTests/Category Cahce/CacheCategoriesUseCaseTests.swift
+++ accenture-assignmentTests/Category Cahce/CacheCategoriesUseCaseTests.swift
@@ -2,13 +2,23 @@
 import XCTest
 
 class LocalCategoryLoader {
+    private let store: CategoryStore
+    
     init(store: CategoryStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ categories: [CategoryItem]) {
+        store.deleteCachedCategories()
     }
 }
 
 class CategoryStore {
     var deleteCachedCategoriesCallCount = 0
+    
+    func deleteCachedCategories() {
+        deleteCachedCategoriesCallCount += 1
+    }
 }
 
 final class CacheCategoriesUseCaseTests: XCTestCase {
@@ -18,5 +28,21 @@ final class CacheCategoriesUseCaseTests: XCTestCase {
         _ = LocalCategoryLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedCategoriesCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = CategoryStore()
+        let sut = LocalCategoryLoader(store: store)
+        let items = [uniqueCategory(), uniqueCategory()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedCategoriesCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueCategory() -> CategoryItem {
+        CategoryItem(id: 1, name: "any")
     }
 }
