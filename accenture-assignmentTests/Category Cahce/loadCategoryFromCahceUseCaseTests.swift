72fe2833b6cc0b9ee43d6c0f9f9e569aa2de1a2c
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 22:58:11 +0300

    Load command does not delete less than seven days old cache

--- accenture-assignmentTests/Category Cahce/loadCategoryFromCahceUseCaseTests.swift
+++ accenture-assignmentTests/Category Cahce/loadCategoryFromCahceUseCaseTests.swift
@@ -88,6 +88,18 @@ final class loadCategoryFromCahceUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+        let categories = uniqueCategories()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysoldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.load() { _ in}
+        store.completeRetrieval(with: categories.local, timestamp: lessThanSevenDaysoldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalCategoryLoader, store: CategoryStoreSpy) {
