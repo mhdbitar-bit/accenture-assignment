7989fccae7e7e096da9f776773314df28905232a
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-02 22:56:38 +0300

    Load command does not delete chase when cache is already empty

--- accenture-assignmentTests/Category Cahce/loadCategoryFromCahceUseCaseTests.swift
+++ accenture-assignmentTests/Category Cahce/loadCategoryFromCahceUseCaseTests.swift
@@ -79,6 +79,15 @@ final class loadCategoryFromCahceUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCahcedCategories])
     }
     
+    func test_load_doesNotDeleteCacheOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        sut.load() { _ in}
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalCategoryLoader, store: CategoryStoreSpy) {
