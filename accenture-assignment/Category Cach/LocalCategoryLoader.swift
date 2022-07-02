5eef4e20017ac332898787701f606313ff6ea3da
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 00:03:52 +0300

    Make `CategoriesCahcePolicy` static since it doesn't keep any state

--- accenture-assignment/Category Cach/LocalCategoryLoader.swift
+++ accenture-assignment/Category Cach/LocalCategoryLoader.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 private final class CategoriesCahcePolicy {
-    private let calendar = Calendar(identifier: .gregorian)
+    private init() {}
     
-    private var maxCacheAgeInDays: Int {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
         return 7
     }
     
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
@@ -25,7 +27,6 @@ private final class CategoriesCahcePolicy {
 final class LocalCategoryLoader {
     private let store: CategoryStore
     private let currentDate: () -> Date
-    private let cachePloicy = CategoriesCahcePolicy()
     
     init(store: CategoryStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -67,7 +68,7 @@ extension LocalCategoryLoader: CategoryLoader {
             case let .failure(error):
                 completion(.failure(error))
             
-            case let .found(categories, timestamp) where self.cachePloicy.validate(timestamp, against: self.currentDate()):
+            case let .found(categories, timestamp) where CategoriesCahcePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(categories.toModels()))
                 
             case .found, .empty:
@@ -86,7 +87,7 @@ extension LocalCategoryLoader {
             case .failure:
                 self.store.deleteCachedCategories { _ in }
                 
-            case let .found(_, timestamp) where !self.cachePloicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !CategoriesCahcePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedCategories { _ in }
                 
             case .empty, .found: break
