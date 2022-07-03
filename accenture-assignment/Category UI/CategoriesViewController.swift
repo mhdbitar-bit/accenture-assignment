37a072106b123acb7d8ecc237c287cf7062abe50
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 11:44:26 +0300

    Render loaded categories on successful load completion

--- accenture-assignment/Category UI/CategoriesViewController.swift
+++ accenture-assignment/Category UI/CategoriesViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class CategoriesViewController: UITableViewController {
     private var loader: CategoryLoader?
-         
+    private var tablewModel = [CategoryItem]()
+    
     convenience init(loader: CategoryLoader) {
         self.init()
         self.loader = loader
@@ -25,8 +26,67 @@ final class CategoriesViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
+            self?.tablewModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
             self?.refreshControl?.endRefreshing()
         }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tablewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tablewModel[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = cellModel.name
+        return cell
+    }
 }
+
+//final class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//
+//    private let tableView: UITableView = {
+//        let table = UITableView()
+//        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        return table
+//    }()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//        view.addSubview(tableView)
+//
+//        tableView.refreshControl = UIRefreshControl()
+//        tableView.refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+//    }
+//
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//
+//        tableView.frame = view.bounds
+//    }
+//
+//    @objc private func refresh() {
+//        // TODO
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//
+//        cell.textLabel?.text = "Helo"
+//
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+//}
--- accenture-assignmentUITests/CategoriesViewControllerTests.swift
+++ accenture-assignmentUITests/CategoriesViewControllerTests.swift
@@ -35,6 +35,22 @@ final class CategoriesViewControllerTests: XCTestCase {
         XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading is completed")
     }
     
+    func test_loadCategoriesCompletion_rendersSuccessfullyloadedCategories() {
+        let cat1 = makeCategory(id: 0, name: "a name")
+        let cat2 = makeCategory(id: 1, name: "another name")
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        assertThat(sut, isRendering: [])
+        
+        loader.completeCategoriesLoading(with: [cat1], at: 0)
+        assertThat(sut, isRendering: [cat1])
+        
+        sut.simulateUserInitiatedCategoriesReload()
+        loader.completeCategoriesLoading(with: [cat1, cat2], at: 1)
+        assertThat(sut, isRendering: [cat1, cat2])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: CategoriesViewController, loader: LoaderSpy) {
@@ -43,6 +59,30 @@ final class CategoriesViewControllerTests: XCTestCase {
         trackForMemoryLeacks(loader, file: file, line: line)
         trackForMemoryLeacks(sut, file: file, line: line)
         return (sut, loader)
+    }
+    
+    private func assertThat(_ sut: CategoriesViewController, isRendering categories: [CategoryItem], file: StaticString = #filePath, line: UInt = #line) {
+        guard sut.numberOfRenderedCategoriesViews() == categories.count else {
+            return XCTFail("Expected \(categories.count) categories, got \(sut.numberOfRenderedCategoriesViews()) instead", file: file, line: line   )
+        }
+        
+        categories.enumerated().forEach { index, category in
+            assertThat(sut, hasViewConfiguredFor: category, at: index, file: file, line: line)
+        }
+    }
+    
+    private func assertThat(_ sut: CategoriesViewController, hasViewConfiguredFor cateogry: CategoryItem, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
+        let view = sut.categoryView(at: index)
+        
+        guard let cell = view else {
+            return XCTFail("Expected \(UITableViewCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
+        }
+
+        XCTAssertEqual(cell.nameText, cateogry.name, "Expected name  text to be \(String(describing: cateogry.name)) for category view at index \(index)", file: file, line: line)
+    }
+    
+    private func makeCategory(id: Int, name: String) -> CategoryItem {
+        return CategoryItem(id: id, name: name)
     }
     
     class LoaderSpy: CategoryLoader {
@@ -56,8 +96,8 @@ final class CategoriesViewControllerTests: XCTestCase {
             completions.append(completion)
         }
         
-        func completeCategoriesLoading(at index: Int) {
-            completions[index](.success([]))
+        func completeCategoriesLoading(with categories: [CategoryItem] = [], at index: Int = 0) {
+            completions[index](.success(categories))
         }
     }
 }
@@ -69,6 +109,26 @@ private extension CategoriesViewController {
     
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
+    }
+    
+    func numberOfRenderedCategoriesViews() -> Int {
+        return tableView.numberOfRows(inSection: categoriesSection)
+    }
+    
+    func categoryView(at row: Int) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: categoriesSection)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
+    
+    private var categoriesSection: Int {
+        return 0
+    }
+}
+
+private extension UITableViewCell {
+    var nameText: String {
+        return textLabel?.text ?? ""
     }
 }
 
