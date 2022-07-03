c0584583762677fc1e385897a221961fcede69b9
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 10:04:18 +0300

    Remove main storyboard

--- accenture-assignment/SceneDelegate.swift
+++ accenture-assignment/SceneDelegate.swift
@@ -8,13 +8,44 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    private let navigationController = UINavigationController()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        configureWindow()
     }
-
+    
+    func configureWindow() {
+        navigationController.setViewControllers([makeRootViewController()], animated: false)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func makeRootViewController() -> CategoriesViewController {
+        return CategoriesViewController()
+//        let client = URLSessionHTTPClient(session: .shared)
+//        let remoteService = RemotePhotoService(client: client)
+//        let imageService = RemoteImageDataService(client: client)
+//
+//        let localStoreURL = NSPersistentContainer
+//            .defaultDirectoryURL()
+//            .appendingPathComponent("image-search-store.sqlite")
+//
+//        let localStore = try! CoreDataSearchStore(storeURL: localStoreURL)
+//        let keywordsService = SearchKeywordService(store: localStore)
+//
+//        let photoOperation = Operations()
+//        let viewModel = PhotoListViewModel(
+//            remoteService: remoteService,
+//            imageService: imageService,
+//            searchKeywordService: keywordsService,
+//            photoOperation: photoOperation)
+//        let vc = PhotoListViewController(viewModel: viewModel)
+//        return vc
+    }
 }
 
