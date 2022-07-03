26de055ab644310d079f9fb12fc1d02cd9231f24
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 18:30:38 +0300

    Add BookLoader

---
+++ accenture-assignment/Features/Book Feature/BookLoader.swift
@@ -0,0 +1,16 @@
+//
+//  BookLoader.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/3/22.
+//
+
+import Foundation
+
+import Foundation
+
+typealias LoadBookResult = Result<[BookItem], Error>
+
+protocol BookLoader {
+    func load(completion: @escaping (LoadBookResult) -> Void)
+}
