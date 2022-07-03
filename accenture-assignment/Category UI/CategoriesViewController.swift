490c210c762982a0a70b721f7d9d20b6ccdcaffc
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 10:04:07 +0300

    Create `CategoriesViewController`

---
+++ accenture-assignment/Category UI/CategoriesViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CategoriesViewController.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/3/22.
+//
+
+import UIKit
+
+final class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .red
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Helo"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
