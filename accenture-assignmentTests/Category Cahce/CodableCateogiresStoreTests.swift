3becea7c234a38078b3283fde0984017f6655abd
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 08:19:38 +0300

    Extract duplicate no side-effects on-retrieve test code into a reusable helper method

--- accenture-assignmentTests/Category Cahce/CodableCateogiresStoreTests.swift
+++ accenture-assignmentTests/Category Cahce/CodableCateogiresStoreTests.swift
@@ -72,23 +72,8 @@ final class CodableCateogiresStoreTests: XCTestCase {
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.retrieve { firstResult in
-            sut.retrieve { secondResult in
-                switch (firstResult, secondResult) {
-                case (.empty, .empty):
-                    break
-                    
-                default:
-                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
-                }
-                
-                exp.fulfill()
-            }
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieveTwice: .empty)
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
@@ -114,31 +99,12 @@ final class CodableCateogiresStoreTests: XCTestCase {
         
         sut.insert(categories, timestamp: timestamp) { insertionError in
             XCTAssertNil(insertionError, "Expected categories to be inserted successfully")
-            
-            sut.retrieve { firstResult in
-                sut.retrieve { secondResult in
-                switch (firstResult, secondResult) {
-                case let (.found(firstFound), .found(secondFound)):
-                    XCTAssertEqual(firstFound.categories, categories)
-                    XCTAssertEqual(firstFound.timestamp, timestamp)
-
-                    XCTAssertEqual(secondFound.categories, categories)
-                    XCTAssertEqual(secondFound.timestamp, timestamp)
-
-                    default:
-                        XCTFail("Expected retrieving twice from non empty caceh to deliver same found result categories \(categories) and \(timestamp), got \(firstResult) and \(secondResult) instead")
-                    }
-                
-                    exp.fulfill()
-                
-                }
-            }
+            exp.fulfill()
         }
-        
         wait(for: [exp], timeout: 1.0)
+    
+        expect(sut, toRetrieveTwice: .found(categories: categories, timestamp: timestamp))
     }
-    
-    
     
     // MARK: - Helpers
     
@@ -146,6 +112,11 @@ final class CodableCateogiresStoreTests: XCTestCase {
         let sut = CodableCategoriesStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeacks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: CodableCategoriesStore, toRetrieveTwice expectedResult: RetrieveCachedCategoriesResult, file: StaticString = #filePath, line: UInt = #line) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
     
     private func expect(_ sut: CodableCategoriesStore, toRetrieve expectedResult: RetrieveCachedCategoriesResult, file: StaticString = #filePath, line: UInt = #line) {
