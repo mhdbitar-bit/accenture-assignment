ec48fa5cfe705c3e5344527caa126812265a0fd5
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 10:38:07 +0300

    Loads categories on view did load

--- accenture-assignmentUITests/CategoriesViewControllerTests.swift
+++ accenture-assignmentUITests/CategoriesViewControllerTests.swift
@@ -1,9 +1,19 @@
 @testable import accenture_assignment
 import XCTest
+import UIKit
 
-class CategoriesViewController {
-    init(loader: CategoriesViewControllerTests.LoaderSpy) {
+class CategoriesViewController: UIViewController {
+    private var loader: CategoriesViewControllerTests.LoaderSpy?
+         
+    convenience init(loader: CategoriesViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
@@ -11,14 +21,27 @@ final class CategoriesViewControllerTests: XCTestCase {
 
     func test_init_doesNoLoadCategories() {
         let loader = LoaderSpy()
-        let sut = CategoriesViewController(loader: loader)
+        _ = CategoriesViewController(loader: loader)
         
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_loadsCategories() {
+        let loader = LoaderSpy()
+        let sut = CategoriesViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
     
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
