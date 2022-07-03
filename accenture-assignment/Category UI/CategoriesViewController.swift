a1a3d133c3eed8fb0a1eb87c3394944156b6c158
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-03 11:15:23 +0300

    Move FeedViewController to production target

--- accenture-assignment/Category UI/CategoriesViewController.swift
+++ accenture-assignment/Category UI/CategoriesViewController.swift
@@ -7,48 +7,26 @@
 
 import UIKit
 
-final class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    private let tableView: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        return table
-    }()
+final class CategoriesViewController: UITableViewController {
+    private var loader: CategoryLoader?
+         
+    convenience init(loader: CategoryLoader) {
+        self.init()
+        self.loader = loader
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        view.addSubview(tableView)
-        
-        tableView.refreshControl = UIRefreshControl()
-        tableView.refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        tableView.frame = view.bounds
-    }
-    
-    @objc private func refresh() {
-        // TODO
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = "Helo"
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
 }
--- accenture-assignmentUITests/CategoriesViewControllerTests.swift
+++ accenture-assignmentUITests/CategoriesViewControllerTests.swift
@@ -2,30 +2,6 @@
 import XCTest
 import UIKit
 
-class CategoriesViewController: UITableViewController {
-    private var loader: CategoryLoader?
-         
-    convenience init(loader: CategoryLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
-
 final class CategoriesViewControllerTests: XCTestCase {
 
     func test_loadCategoriesActions_requestCategoreisFromLoader() {
