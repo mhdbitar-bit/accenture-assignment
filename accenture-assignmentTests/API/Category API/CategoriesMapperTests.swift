652f1bbba9c76a02ce83e3239fefe4b074d7c1b9
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-04 19:20:16 +0300

    Test CategoryMapper in isolation

--- accenture-assignmentTests/API/Category API/CategoriesMapperTests.swift
+++ accenture-assignmentTests/API/Category API/CategoriesMapperTests.swift
@@ -3,62 +3,44 @@ import XCTest
 
 final class CategoriesMapperTests: XCTestCase {
 
-    func test_load_deliversErrorOnNon200HTTPResponse() {
-        let (sut, client) = makeSUT()
-        
+    func test_map_deliversErrorOnNon200HTTPResponse() throws {
+        let json = makeItemsJSON([])
         let samples = [199, 201, 300, 400, 500]
         
-        samples.enumerated().forEach { index, code in
-            expcat(sut, toCompleteWith: failure(.invalidData)) {
-                let json = makeItemsJSON([])
-                client.complete(withStatusCode: code, data: json, at: index)
-            }
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try CategoryItemsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
     
-    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
-        let (sut, client) = makeSUT()
+    func test_map_deliversErrorOn200HTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
         
-        expcat(sut, toCompleteWith: failure(.invalidData)) {
-            let invalidJSON = Data("invalid json".utf8)
-            client.complete(withStatusCode: 200, data: invalidJSON)
-        }
+        XCTAssertThrowsError(
+            try CategoryItemsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
+        )
     }
     
-    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONlist() {
-        let (sut, client) = makeSUT()
-
-        expcat(sut, toCompleteWith: .success([])) {
-            let emptyListJSON = makeItemsJSON([])
-            client.complete(withStatusCode: 200, data: emptyListJSON)
-        }
+    func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONlist() throws {
+        let emptyListJSON = makeItemsJSON([])
+        
+        let result = try CategoryItemsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [])
     }
     
-    func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
-        let (sut, client) = makeSUT()
-
+    func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
         let item1 = makeItem(id: 1, name: "a name")
         let item2 = makeItem(id: 2, name: "another name")
+        let json = makeItemsJSON([item1.json, item2.json])
         
-        expcat(sut, toCompleteWith: .success([item1.model, item2.model])) {
-            let json = makeItemsJSON([item1.json, item2.json])
-            client.complete(withStatusCode: 200, data: json)
-        }
+        let result = try CategoryItemsMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [item1.model, item2.model])
     }
     
     // MARK: - Helpers
-    
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCategoryLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteCategoryLoader(url: url, client: client)
-        trackForMemoryLeacks(sut, file: file, line: line)
-        trackForMemoryLeacks(client, file: file, line: line)
-        return (sut, client)
-    }
-    
-    private func failure(_ error: RemoteCategoryLoader.Error) -> RemoteCategoryLoader.Result {
-        return .failure(error)
-    }
     
     private func makeItem(id: Int, name: String) -> (model: CategoryItem, json: [String: Any]) {
         
@@ -74,28 +56,5 @@ final class CategoriesMapperTests: XCTestCase {
     
     private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
         return try! JSONSerialization.data(withJSONObject: items)
-    }
-    
-    private func expcat(_ sut: RemoteCategoryLoader, toCompleteWith expectedResult: RemoteCategoryLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-                
-            case let (.failure(receivedError as RemoteCategoryLoader.Error), .failure(expectedError as RemoteCategoryLoader.Error)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-       
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
     }
 }
