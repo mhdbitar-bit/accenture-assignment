7fd98b94c5fc7fd354f9431c98824f36337fa19c
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-05 22:02:58 +0300

    Add BookTableCell

---
+++ accenture-assignment/UI/Book UI/BookTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookTableViewCell.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/5/22.
+//
+
+import UIKit
+
+final class BookTableViewCell: UITableViewCell {
+    @IBOutlet private(set) var bookTitleLabel: UILabel!
+    @IBOutlet private(set) var authorLabel: UILabel!
+    @IBOutlet private(set) var releaseDateLabel: UILabel!
+    @IBOutlet private(set) var noPagesLabel: UILabel!
+}
