96e6b6c38f0aac0b7fb09500fb630169490cdcf5
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-05 22:03:44 +0300

    Add CharacterCell

---
+++ accenture-assignment/UI/Character UI/CharacterTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterTableViewCell.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/5/22.
+//
+
+import UIKit
+
+final class CharacterTableViewCell: UITableViewCell {
+    @IBOutlet private(set) var characterNameLabel: UILabel!
+    @IBOutlet private(set) var actorLabel: UILabel!
+}
