7ee597d2fdbcd0ca25715b0b218e03780721d426
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-05 22:03:24 +0300

    Add HouseCell

---
+++ accenture-assignment/UI/House UI/HouseTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HouseTableViewCell.swift
+//  accenture-assignment
+//
+//  Created by Mohammad Bitar on 7/5/22.
+//
+
+import UIKit
+
+final class HouseTableViewCell: UITableViewCell {
+    @IBOutlet private(set) var houseImage: UIImageView!
+    @IBOutlet private(set) var houseNameLabel: UILabel!
+    @IBOutlet private(set) var houseTitleLabel: UILabel!
+}
