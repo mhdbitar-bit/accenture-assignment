d2ac62a12c1a091366e161653db147f79c06b42c
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-08 12:24:01 +0300

    Add computed property to CategoryItem

--- accenture-assignment/Features/Category Feature/CategoryItem.swift
+++ accenture-assignment/Features/Category Feature/CategoryItem.swift
@@ -11,8 +11,26 @@ struct CategoryItem: Equatable {
     let id: Int
     let name: String
     
+    var category: Category {
+        switch id {
+        case 1:
+            return .Books
+        case 2:
+            return .Houses
+        case 3:
+            return .Characters
+        default: return .Books
+        }
+    }
+    
     init(id: Int, name: String) {
         self.id = id
         self.name = name
     }
+}
+
+enum Category: Int {
+    case Books
+    case Houses
+    case Characters
 }
